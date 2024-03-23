2f85f6f112b5d3c7244614dfc2710fe2d62bf1ed
Can Rager <web.rager@posteo.de>
2024-03-23 02:12:06 +0100

    added homepage multiple databases

---
+++ render_utils.py
@@ -0,0 +1,38 @@
+def tokens_to_html(tokens, max_len=150):
+    """Given a list of tokens (strings), returns html for displaying the tokenized text.
+    """
+    newline_tokens = ['\n', '\r', '\r\n', '\v', '\f']
+    html = ""
+    txt = ""
+    if len(tokens) > max_len:
+        html += '<span>...</span>'
+    tokens = tokens[-max_len:]
+    for i, token in enumerate(tokens):
+        background_color = "white" if i != len(tokens) - 1 else "#FF9999"
+        txt += token
+        if all([c in newline_tokens for c in token]):
+            # replace all instances with ⏎
+            token_rep = len(token) * "⏎"
+            brs = "<br>" * len(token)
+            html += f'<span style="border: 1px solid #DDD; background-color: {background_color}; white-space: pre-wrap;">{token_rep}</span>{brs}'
+        else:
+            # replace any $ with \$ to avoid markdown interpretation
+            token = token.replace("$", "\$")
+            # replace any < with &lt; to avoid html interpretation
+            # token = token.replace("<", "&lt;")
+            # replace any > with &gt; to avoid html interpretation
+            # token = token.replace(">", "&gt;")
+            # replace any & with &amp; to avoid html interpretation
+            token = token.replace("&", "&amp;")
+            # replace any _ with \_ to avoid markdown interpretation
+            token = token.replace("_", "\_")
+            # also escape * to avoid markdown interpretation
+            token = token.replace("*", "\*")
+            # there's also an issue with the backtick, so escape it
+            token = token.replace("`", "\`")
+
+            html += f'<span style="border: 1px solid #DDD; background-color: {background_color}; white-space: pre-wrap;">{token}</span>'
+    if "</" in txt:
+        return "CONTEXT NOT LOADED FOR SECURITY REASONS SINCE IT CONTAINS HTML CODE (could contain javascript)."
+    else:
+        return html
