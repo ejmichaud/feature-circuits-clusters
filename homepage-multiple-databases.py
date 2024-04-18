5c93f9230e2b104b8c9ffa29f4ba8ee034a96f9b
saprmarks <saprmarks@gmail.com>
2024-04-17 22:49:48 -0400

    fixing bug in quick lists

--- homepage-multiple-databases.py
+++ homepage-multiple-databases.py
@@ -338,7 +338,7 @@ if 'circuit_graphviz' in cluster_data and cluster_data['circuit_graphviz'] is no
             pass
     # create neuronpedia link
     url = "https://neuronpedia.org/quick-list/"
-    name = urllib.parse.quote(LIST_NAME)
+    name = urllib.parse.quote(features)
     url = url + "?name=" + name
     url = url + "&features=" + urllib.parse.quote(json.dumps(features))
     st.markdown(f"[View this circuit's features on Neuronpedia]({url})")
